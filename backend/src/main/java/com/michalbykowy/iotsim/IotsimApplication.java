649741166349b57a76c82f7763fa1f8bb0c0fb8a
MBykowy <154358334+MBykowy@users.noreply.github.com>
2025-10-07 21:11:16 +0200

    Initial commit of backend and frontend projects

---
+++ backend/src/main/java/com/michalbykowy/iotsim/IotsimApplication.java
@@ -0,0 +1,13 @@
+package com.michalbykowy.iotsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotsimApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/michalbykowy/iotsim/IotsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalbykowy.iotsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
